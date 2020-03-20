7576896bc8bb54c5c73191bee31eb775d5294358
adriansuszek <adrian.suszek93@gmail.com>
2020-03-20 10:15:36 +0100

    layout personalization, add post app

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, blog
+from .views import index, blog, contact
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
-    path('blog/', blog)
+    path('blog/', blog),
+    path('contact/', contact)
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- src/blog/views.py
+++ src/blog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html', {})
 
-
 def blog(request):
     return render(request, 'blog.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
